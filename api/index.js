78312443e500a4cef59919d8046c60dfb799f3d5
Nishant Makwana <nishantmakwanacreations@gmail.com>
2024-12-20 12:43:10 +0530

    CHANGES

--- api/index.js
+++ api/index.js
@@ -193,7 +193,7 @@ app.delete("/profile/cart/:itemId", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server Is Running On : http://localhost:${PORT}`);
 });
